fd204019fe4d6e4349639f9a4c290370e9f56b06
ArseniyKhom <Arseny.Khomuskov@urfu.me>
2022-12-31 10:30:04 +0500

    Revert "3.5.1"
    This reverts commit bc3293ede188721faf934ae4662ffaf778d0d1ef.

--- 3.5.1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sqlite3
-
-df = pd.read_csv("3.3.1.csv")
-
-connection = sqlite3.connect("3_5_1.db")
-cursor = connection.cursor()
-df.to_sql(name="currency_from_api", con=connection, if_exists='replace', index=False)
-connection.commit()
